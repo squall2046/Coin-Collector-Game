7212b01e87577eda3e9779442f47197f4dafbb93
squall2046 <squall2046@hotmail.com>
2018-11-22 22:58:20 -0500

    Coin Collector Version1, correct functions, add pictures,audios

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,6 @@
 // Isaac Wu Crystal Collector Game
 $(document).ready(function () {
 
-    // function reset() {
     var winTimes = 0;
     var lossTimes = 0;
     var totalScore = 0;
@@ -16,66 +15,92 @@ $(document).ready(function () {
     console.log(coinNum3);
     var coinNum4 = Math.floor(Math.random() * 2) + 1;
     console.log(coinNum4);
-    // }
 
-    //==== Box1 random coin:
-    $("#random-number").append(randomNum);
-
-    //==== Box2 total score & Box-set questions/coins:
-    // #coin1
-    $("#coin1").on("click", function () {
-        totalScore = totalScore + coinNum1;
-        console.log(totalScore);
-        $("#total-score").append(totalScore);
-    })
-    // #coin2
-    $("#coin2").on("click", function () {
-        totalScore = totalScore + coinNum2;
-        console.log(totalScore);
-        $("#total-score").append(totalScore);
-    })
-    // #coin3
-    $("#coin3").on("click", function () {
-        totalScore = totalScore + coinNum3;
-        console.log(totalScore);
-        $("#total-score").append(totalScore);
-    })
-    // #coin4
-    $("#coin4").on("click", function () {
-        totalScore = totalScore + coinNum4;
-        console.log(totalScore);
-        $("#total-score").append(parseInt(totalScore));
-    })
-    //#clear
-    $("#clear").on("click", function () {
-        winTimes = 0;
-        lossTimes = 0;
-        totalScore = 0;
+    function reset (){
         randomNum = Math.floor(Math.random() * 101) + 19;
         coinNum1 = Math.floor(Math.random() * 4) + 9;
         coinNum2 = Math.floor(Math.random() * 3) + 6;
         coinNum3 = Math.floor(Math.random() * 3) + 3;
         coinNum4 = Math.floor(Math.random() * 2) + 1;
-        $("#coin1, #coin2, #coin3, #coin4, #total-score").empty();
+        $("#random-number").text(randomNum);
+        totalScore = 0;
+        $("#total-score").empty();
+    }
+
+    //==== Box1 random coin:
+    $("#random-number").text(randomNum);
+
+    //==== Box2 total score & Box-set questions/coins:
+    // #coin1
+    $("#coin1").on("click", function () {
+        totalScore += coinNum1; // totalScore = totalScore + coinNum1;
+        console.log(totalScore);
+        $("#total-score").text(totalScore);
+        $("#mariocontain").html('<img src="assets/images/coin3.gif" class="mariopic">');
+        var boxSound = new Audio('assets/sound/smb_coin.mp3');
+        boxSound.play();
+        decide();
+    })
+    // #coin2
+    $("#coin2").on("click", function () {
+        totalScore += coinNum2;
+        console.log(totalScore);
+        $("#total-score").text(totalScore);
+        $("#mariocontain").html('<img src="assets/images/coin3.gif" class="mariopic">');
+        boxSound = new Audio('assets/sound/smb_coin.mp3');
+        boxSound.play();
+        decide();
+    })
+    // #coin3
+    $("#coin3").on("click", function () {
+        totalScore += coinNum3;
+        console.log(totalScore);
+        $("#total-score").text(totalScore);
+        $("#mariocontain").html('<img src="assets/images/coin3.gif" class="mariopic">');
+        boxSound = new Audio('assets/sound/smb_coin.mp3');
+        boxSound.play();
+        decide();
+    })
+    // #coin4
+    $("#coin4").on("click", function () {
+        totalScore += coinNum4;
+        console.log(totalScore);
+        $("#total-score").text(parseInt(totalScore));
+        $("#mariocontain").html('<img src="assets/images/coin3.gif" class="mariopic">');
+        boxSound = new Audio('assets/sound/smb_coin.mp3');
+        boxSound.play();
+        decide();
+    })
+    //#clear
+    $("#clear").on("click", function () {
+        reset();
+        var clearSound = new Audio('assets/sound/smb_bricksmash.mp3');
+        clearSound.play();
     });
 
-    //==== Box3 condition wins/losses times:
+    // totalScore = randomNum;
+
+    //==== Box3 condition wins/losses times & Box4 mario coin picture:
+    function decide(){
     if (totalScore === randomNum) {
         winTimes++;
-        $("#result").text("Yes You Win !");
+        $("#wins").text(winTimes);
+        $("#result").text("You Win !");
+        $("#mariocontain").html('<img src="assets/images/coin4.gif" class="mariopic">');
+        var winSound = new Audio('assets/sound/sm64_1-up.mp3');
+        winSound.play();
+        reset();
     } else if (totalScore > randomNum) {
         lossTimes++;
+        $("#losses").text(lossTimes);
         $("#result").text("Oh No !");
+        $("#mariocontain").html('<img src="assets/images/gameover.jpg" class="mariopic">');
+        var lossSound = new Audio('assets/sound/smb_mariodie.mp3');
+        lossSound.play();
+        reset();
     } else {
-        $("#result").text("Here we go !");
+        $("#result").text("");
     }
-    // #wins
-    $("#wins").append(winTimes);
-    // #losses
-    $("#losses").append(lossTimes);
+}
 
-    // totalScore = randomNum;
-    // $("#total-score").append(totalScore);
-
-    //==== Box4 mario coin picture:
 });
